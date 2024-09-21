d90a96ccdf96e9f51a544e87a424e7d65033590b
bsadriano <bryanadriano018@gmail.com>
2024-09-21 13:52:44 +0800

    Add health exports

---
+++ .changeset/wild-ghosts-sneeze.md
@@ -0,0 +1,5 @@
+---
+'@bsadriano/rmq-nestjs-lib': patch
+---
+
+Add health exports
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './auth';
 export * from './constants';
 export * from './database';
+export * from './health';
 export * from './interceptors';
 export * from './queue';
 export * from './rmq';
